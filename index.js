0c78bf0cebabaa2577b64387bc9473319fd5a65c
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-10 19:27:52 -0400

    Array

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards (arr,event) {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+          
+    }
+   return messages
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown(){
+for (let countup = 0; countup <= 10; countup++) {
+    console.log(countup);
+  }
+}
+ 
